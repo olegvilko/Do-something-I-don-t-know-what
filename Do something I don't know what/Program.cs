97bfc101ee06d9162dd066627d8e0714e247afd0
vilko <helgi8@mail.ru>
2018-10-12 15:40:00 +0300

    Добавьте файлы проекта.

---
+++ Do something I don't know what/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Do_something_I_don_t_know_what
+{
+    class Program
+    {
+        static int Decode(string str)
+        {
+            str = str.Replace(".", string.Empty);
+            int num = Int32.Parse(str);
+            num = num % 1024;
+            return num;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Decode("1...2"));
+        }
+    }
+}
